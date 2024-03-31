9fe8c89b8c439caa9e6a01692c0622c43852d483
CrossTech <526383173@qq.com>
2024-03-31 10:57:32 +0800

    Refactor syntax of airport mapping to make it more concise and less readable

--- app/src/main/java/crosstech/aviaassist/ui/FlightViewModel.kt
+++ app/src/main/java/crosstech/aviaassist/ui/FlightViewModel.kt
@@ -15,13 +15,13 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 
-class FlightViewModel: ViewModel() {
+class FlightViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(FlightUiState())
     val uiState: StateFlow<FlightUiState> = _uiState.asStateFlow()
     lateinit var flightDataList: List<FlightData>
     lateinit var airportMap: Map<String, String>
 
-    fun uploadMissionsGroupedByDate(missionsText: String){
+    fun uploadMissionsGroupedByDate(missionsText: String) {
         val missions: List<FlightMission> = FlightMission.parseListFromString(missionsText)
         val groupedMission = missions.groupBy { it.takeoffDateTime.toLocalDate() }
         _uiState.update {
@@ -29,7 +29,7 @@ class FlightViewModel: ViewModel() {
         }
     }
 
-    fun initFlightDataAndAirportMap(context: Context){
+    fun initFlightDataAndAirportMap(context: Context) {
         val documentStream = context.resources.openRawResource(R.raw.flight_data)
         val bufferedReader = BufferedReader(InputStreamReader(documentStream))
 
@@ -42,16 +42,12 @@ class FlightViewModel: ViewModel() {
                 if (fltDatum != null) {
                     flightDataMutableList.add(fltDatum)
                 }
-                val airportPair = FlightData.parseAirports(line)
-                if (!airports.containsKey(airportPair?.first?.first)){
-                    if (airportPair != null) {
-                        airports[airportPair.first.first] = airportPair.first.second
-                    }
-                }
-                if (!airports.containsKey(airportPair?.second?.first)){
-                    if (airportPair != null) {
-                        airports[airportPair.second.first] = airportPair.second.second
-                    }
+
+                FlightData.parseAirports(line)?.let {
+                    if (airports.containsKey(it.first.first))
+                        airports[it.first.first] = it.first.second
+                    if (airports.containsKey(it.second.first))
+                        airports[it.second.first] = it.second.second
                 }
                 // Next line
                 line = bufferedReader.readLine()
