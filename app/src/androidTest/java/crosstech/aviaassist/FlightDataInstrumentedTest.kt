6282eeae18421f1800a89d1f588dc6ea002fcaea
CrossTech <526383173@qq.com>
2024-03-31 09:52:00 +0800

    Implement and test flight data parsing

--- app/src/androidTest/java/crosstech/aviaassist/FlightDataInstrumentedTest.kt
+++ app/src/androidTest/java/crosstech/aviaassist/FlightDataInstrumentedTest.kt
@@ -1,7 +1,9 @@
 package crosstech.aviaassist
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import crosstech.aviaassist.ui.FlightViewModel
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,11 +16,20 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class FlightDataInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("crosstech.aviaassist", appContext.packageName)
+    }
+
+    @Test
+    fun flightData_Initialization_Successful(){
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val flightVM = FlightViewModel()
+        flightVM.initFlightData(context)
+        Log.d("TEST", flightVM.flightDataList.size.toString())
+        assertNotEquals(0, flightVM.flightDataList.size)
     }
 }
---
+++ app/src/test/java/crosstech/aviaassist/FlightDataUnitTest.kt
@@ -0,0 +1,36 @@
+package crosstech.aviaassist
+
+import crosstech.aviaassist.model.FlightData
+import org.junit.Test
+import org.junit.Assert.*
+
+class FlightDataUnitTest {
+    val raw = buildString {
+        appendLine("杭州,HGH,北京,PEK,319,1.20,136.00")
+        appendLine("杭州,HGH,北京,PEK,320,1.20,136.00")
+        appendLine("杭州,HGH,北京,PEK,737,1.20,136.00")
+        appendLine("杭州,HGH,北京,PEK,767,1.20,130.00")
+        appendLine("杭州,HGH,广州,CAN,319,1.20,124.00")
+        appendLine("杭州,HGH,广州,CAN,320,1.20,124.00")
+        appendLine("杭州,HGH,贵阳,KWE,319,1.20,147.00")
+        appendLine("杭州,HGH,贵阳,KWE,320,1.20,147.00")
+        appendLine("杭州,HGH,哈尔滨,HRB,319,1.00,185.00")
+        appendLine("杭州,HGH,海口,HAK,319,1.00,168.00")
+        appendLine("杭州,HGH,昆明,KMG,320,1.20,175.00")
+        appendLine("杭州,HGH,北京,PEK,330,1.20,140.00")
+        appendLine("杭州,HGH,深圳,SZX,320,1.20,140.00")
+        appendLine("杭州,HGH,西安,XIY,320,1.00,152.00")
+        appendLine("杭州,HGH,成都,CTU,320,1.00,195.00")
+        append("杭州,HGH,重庆,CKG,320,1.00,165.00")
+    }// 11 airports here
+
+    @Test
+    fun flightData_Init_Success() {
+        val rawList = raw.split("\n")
+        val data = mutableListOf<FlightData>()
+        for(str in rawList){
+            FlightData.parseFromString(str)?.let { data.add(it) }
+        }
+        assertEquals(rawList.size, data.size)
+    }
+}
