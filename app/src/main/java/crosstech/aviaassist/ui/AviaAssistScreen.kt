09b4968024408df0bc24da6c739152a6308e7185
CrossTech <526383173@qq.com>
2024-03-31 16:58:33 +0800

    Add app topbar

--- app/src/main/java/crosstech/aviaassist/ui/AviaAssistScreen.kt
+++ app/src/main/java/crosstech/aviaassist/ui/AviaAssistScreen.kt
@@ -1,18 +1,40 @@
 package crosstech.aviaassist.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentPasteGo
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import crosstech.aviaassist.R
+import crosstech.aviaassist.ui.theme.AviaAssistTheme
 
 @Composable
 fun AviaAssistScreen(
@@ -44,9 +66,42 @@ fun AviaAssistScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AviaAssistTopBar() {
+fun AviaAssistTopBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(modifier = modifier,
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(top = dimensionResource(id = R.dimen.padding_small))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.plane_icon), contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(id = R.dimen.image_size))
+                        .padding(dimensionResource(id = R.dimen.padding_small))
+                )
+                Spacer(modifier = Modifier.width(dimensionResource(id = R.dimen.padding_small)))
+                Text(
+                    text = buildAnnotatedString {
+                        pushStyle(SpanStyle(color = MaterialTheme.colorScheme.primary))
+                        append(stringResource(R.string.avia))
+                        pop()
+                        append(stringResource(R.string.assist))
+                    },
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        }
+    )
+}
 
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    AviaAssistTopBar()
 }
 
 @Composable
--- app/src/main/java/crosstech/aviaassist/ui/theme/Type.kt
+++ app/src/main/java/crosstech/aviaassist/ui/theme/Type.kt
@@ -4,13 +4,22 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import crosstech.aviaassist.R
 
+val Pacifico = FontFamily(
+    Font(R.font.pacifico_regular, style = FontStyle.Italic)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
-
+    displayLarge = TextStyle(
+        fontFamily = Pacifico,
+        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Italic,
+        fontSize = 36.sp
+    )
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
