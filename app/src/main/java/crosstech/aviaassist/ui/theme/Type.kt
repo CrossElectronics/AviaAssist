a1d414182ff87f5ceede376628c67bd63226cb47
CrossTech <526383173@qq.com>
2024-03-31 16:24:12 +0800

    Remove font design

--- app/src/main/java/crosstech/aviaassist/ui/theme/Type.kt
+++ app/src/main/java/crosstech/aviaassist/ui/theme/Type.kt
@@ -8,51 +8,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import crosstech.aviaassist.R
 
-val Pacifico = FontFamily(
-    Font(R.font.pacifico_regular)
-)
-val Montserrat = FontFamily(
-    Font(R.font.montserrat_regular),
-    Font(R.font.montserrat_semibold, FontWeight.SemiBold)
-)
-val Din = FontFamily(
-    Font(R.font.din_pro),
-    Font(R.font.din_pro_bold, FontWeight.Bold)
-)
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    ),
-    displayLarge = TextStyle(
-        fontFamily = Pacifico,
-        fontWeight = FontWeight.Normal,
-        fontSize = 36.sp
-    ),
-    headlineLarge = TextStyle(
-        fontFamily = Din,
-        fontWeight = FontWeight.Normal,
-        fontSize = 32.sp
-    ),
-    titleLarge = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp
-    ),
-    titleMedium = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    labelLarge = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    )
 
     /* Other default text styles to override
     titleLarge = TextStyle(
